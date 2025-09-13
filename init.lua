954974f3f15dcf76fb7584f0746239083c68dbf5
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2025-09-13 04:30:06 +0330

    Set tab stop to 2

--- init.lua
+++ init.lua
@@ -75,8 +75,8 @@ end)]]
 vim.o.breakindent = true
 
 vim.o.autoindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 -- Save undo history
 vim.o.undofile = true
