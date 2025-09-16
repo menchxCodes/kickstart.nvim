3c2d48045697f786c56705bd76ce74ab57bf96af
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2025-09-16 18:06:18 +0330

    Add ejs support

--- init.lua
+++ init.lua
@@ -778,6 +778,7 @@ require('lazy').setup({
             'rafamadriz/friendly-snippets',
             config = function()
               require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip').filetype_extend('html', { 'ejs' })
             end,
           },
         },
